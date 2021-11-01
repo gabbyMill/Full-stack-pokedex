948c5d7b3152078c518ab275405a7739c343bf92
gabbyMill <gabbymillatiner@gmail.com>
2021-11-01 21:44:48 +0200

    fix error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,6 +2,7 @@ function errorHandler(err, req, res, next) {
   console.log("in error handler");
   // check for headers ? do it in middleware
   if (!err.status) {
+    console.log(err);
     return res.status(500).json(err.message);
   }
   return res.status(err.status).json(err.message);
